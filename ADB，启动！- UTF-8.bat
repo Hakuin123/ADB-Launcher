a4b0a5e279d81178ab4184fd6e64180cba02ca21
Hakuin123 <108911712+Hakuin123@users.noreply.github.com>
2024-01-16 21:51:45 +0800

    utf8

--- ADB，启动！- UTF-8.bat
+++ ADB，启动！- UTF-8.bat
@@ -1,7 +1,7 @@
 @echo off
-chcp 65001 > nul
-::如果出现乱码请尝试使用GB2312版本
-title ADB，启动！  v1.2  by 白隐Hakuin
+chcp 936 > nul
+::如果出现乱码请尝试使用UTF-8版本
+title ADB，启动！  v1.3  by 白隐Hakuin
 :: 初始化忽略错误变量
 set IGNORE_ERROR=0
 
@@ -9,7 +9,7 @@ set IGNORE_ERROR=0
 echo = 检查 ADB 环境 =
 adb version
 if not %errorlevel%==0 (
-  echo 未找到 ADB（Android Debug Bridge）！请确保这台计算机拥有 ADB 运行环境（可参考网络教程）
+  echo 未找到 ADB（Android Debug Bridge）！请检查此脚本是否和 ADB 相关工具放在同一文件夹下，或确保这台计算机拥有 ADB 运行环境（可参考网络教程）
   echo Android SDK 平台工具：https://googledownloads.cn/android/repository/platform-tools-latest-windows.zip
   echo 请使用浏览器打开上面的链接，下载并解压 'platform-tools' 文件夹，将此.bat文件放在该文件夹下，再次运行此.bat文件
   pause
@@ -72,7 +72,6 @@ if %errorlevel%==0 (
 )
 echo.
 
-goto fix
 
 :scene
 adb shell pm list packages | findstr /i "com.omarea.vtools" > nul
@@ -150,16 +149,24 @@ echo.
 
 
 :end
-echo 全部执行完成，关闭 ADB 服务
+echo == 使用技巧 ==
+echo 1. 若重新插拔 USB 数据线后,无线调试或 Shizuku 等停止，
+echo 请尝试在开发者选项中将“选择 USB 配置”修改为“仅充电”，
+echo 并开启“仅充电模式下允许 ADB 调试”或“USB调试（安全模式）”
+echo.
+echo 2. 若因为网络环境改变使得 Shizuku 服务停止，
+echo 可在 Shizuku 中选择“通过无线调试启动”→“启动”→“5555”
+echo.
+echo == 全部执行完成，关闭 ADB 服务 ==
 adb kill-server
-pause
+echo 请按任意键退出...
+pause > nul
 exit
-
 
 
 :fix
 echo == 执行遇到错误，尝试修复 ==
-echo 尝试关闭正在运行的其它 ADB 进程防止冲突...
+echo 尝试关闭正在运行的其它 ADB.exe 进程防止冲突...
 taskkill /F /IM adb.exe || echo 若提示没有找到进程说明无 ADB 冲突，可忽略
 echo.
 echo 尝试查看 ADB 默认端口（5037）是否被占用...
@@ -167,9 +174,8 @@ netstat -aon | findstr 5037
 echo 若显示结果，则代表该端口可能已被其他进程占用。回显最右侧为该进程对应 PID，请在任务管理器中手动将其关闭
 echo.
 echo == 常见问题 ==
-echo 1. 若提示“'adb' 不是内部或外部命令...”，请确保此脚本是否和 ADB 相关工具放在同一文件夹下，或确保你的计算机是否有 ADB 执行环境（可参考网络教程）
-echo 2. 激活某个应用时若提示“no such file or directory”，请先在设备上启动一次该应用并选择对应的模式
-echo 3. 若提示“adb server version (xxx) doesn't match this client (xxx); killing...”，请关闭计算机上正在运行的“手机助手”类软件，如“360手机助手”“鲁大师手机助手”等
+echo 1. 激活某个应用时若提示“no such file or directory”，请先在设备上启动一次该应用并选择对应的模式
+echo 2. 若提示“adb server version (xxx) doesn't match this client (xxx); killing...”，请关闭计算机上正在运行的“手机助手”类软件，如“360手机助手”“鲁大师手机助手”等
 echo.
 set /p IGNORE_ERROR="请按回车键重新执行，或输入 1 忽略错误并重新执行："
 if "%IGNORE_ERROR%" == "1" (
@@ -178,4 +184,3 @@ if "%IGNORE_ERROR%" == "1" (
 )
 cls
 goto :start
-
